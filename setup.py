3bfe04d0f1870809058997334e798fc44a7e860d
bmsan <compressedsan@gmail.com>
2022-08-04 21:45:54 +0000

    Added code documentation

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="gitstamp",
+    version="0.1.0",
+    author="Bogdan Sandoi",
+    author_email="bogdan.sandoi@gmail.com",
+    description=("Ensure code traceability in ML experiments"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+    url="https://github.com/bmsan/gitstamp",
+)
