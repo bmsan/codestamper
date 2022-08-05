5631756db828ddb01a4026d3d21de41368edf05a
bmsan <compressedsan@gmail.com>
2022-08-05 20:03:29 +0000

    added author to commit command

--- tests/git_init.sh
+++ tests/git_init.sh
@@ -11,13 +11,13 @@ git config user.email "Testing@git.stamp"
 git add file1.py
 git add file2.py
 
-git commit -m "c1"
+git commit -m "c1" --author "Test Bot <testing@git.stamp>"
 
 echo mod1 >> file1.py
 
 git add  file1.py
 
-git commit -m "c2"
+git commit -m "c2" --author "Gogu Gigi <gogu@gigi.com>"
 
 ### 2 commits clean workspace
 
